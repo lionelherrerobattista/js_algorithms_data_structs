c5046d5e35f47be8333fcb801d41bfc43cab6363
Lionel Herrero Battista <lionherrero@gmail.com>
2023-05-25 19:14:39 -0300

    Finished Section 19 - Singly Linked Lists

--- section19_singly_linked_lists/singly_linked_list_course.js
+++ section19_singly_linked_lists/singly_linked_list_course.js
@@ -46,31 +46,100 @@ class SinglyLinkedList {
 		return current;
 	}
 
-    shift(){
-        if(!this.head) return undefined;
-        let currentHead = this.head;
-        this.head = currentHead.next;
-        this.length--;
-        if (this.length === 0) {
+	shift() {
+		if (!this.head) return undefined;
+		let currentHead = this.head;
+		this.head = currentHead.next;
+		this.length--;
+		if (this.length === 0) {
 			this.head = null;
 			this.tail = null;
 		}
-        return currentHead;
-    }
+		return currentHead;
+	}
 
-    unshift(val){
-        let newNode = new Node(val);
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            newNode.next = this.head;
-            this.head = newNode;
-        }
-        this.length++;
-        return this;
-    }
+	unshift(val) {
+		let newNode = new Node(val);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.length++;
+		return this;
+	}
 
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+		let counter = 0;
+		let current = this.head;
+		while (counter !== index) {
+			current = current.next;
+			counter++;
+		}
+		return current;
+	}
+
+	set(index, val) {
+		let foundNode = this.get(index);
+		if (foundNode) {
+			foundNode.val = val;
+			return true;
+		}
+		return false;
+	}
+
+	insert(index, val) {
+		if (index < 0 || index > this.length) return false;
+		if (index === this.length) return !!this.push(val); // converts to boolean
+		if (index === 0) return !!this.unshift(val); // converts to boolean
+
+		let newNode = new Node(val);
+		let prev = this.get(index - 1);
+		let temp = prev.next;
+		prev.next = newNode;
+		newNode.next = temp;
+		this.length++;
+		return true;
+	}
+
+	remove(index) {
+		if (index < 0 || index >= this.length) return undefined;
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+		let previousNode = this.get(index - 1);
+		let removed = previousNode.next;
+		previousNode.next = removed.next;
+		this.length--;
+		return removed;
+	}
+
+	reverse() {
+		let node = this.head;
+		this.head = this.tail;
+		this.tail = node;
+		let next;
+		let prev = null;
+		for (let i = 0; i < this.length; i++) {
+			next = node.next;
+			node.next = prev; // reverse
+			prev = node;
+			node = next; // current is now next
+		}
+		return this;
+	}
+
+	print() {
+		let arr = [];
+		let current = this.head;
+		while (current) {
+			arr.push(current.val);
+			current = current.next;
+		}
+		console.log(arr);
+	}
 }
 
 // Test
@@ -78,6 +147,7 @@ class SinglyLinkedList {
 let list = new SinglyLinkedList();
 list.push("HELLO");
 list.push("GOODBYE");
+list.push("ALL");
 list.push("!");
 
 // Test - Push
@@ -94,17 +164,54 @@ list.push("!");
 // console.log(list);
 
 // Test - Shift
-console.log(list);
-console.log(list.shift());
-console.log(list);
-console.log(list.shift());
-console.log(list);
-console.log(list.shift());
-console.log(list);
-console.log(list.shift());
-console.log(list);
+// console.log(list);
+// console.log(list.shift());
+// console.log(list);
+// console.log(list.shift());
+// console.log(list);
+// console.log(list.shift());
+// console.log(list);
+// console.log(list.shift());
+// console.log(list);
 
 // Test - Unshift
-console.log(list);
-console.log(list.unshift("HI!"));
-console.log(list.unshift(123));
+// console.log(list);
+// console.log(list.unshift("HI!"));
+// console.log(list.unshift(123));
+
+// Test - Get
+// console.log(list.get(1));
+// console.log(list.get(0));
+// console.log(list.get(-1)); // index less than 0
+// console.log(list.get(4)); // index greater than length
+
+// Test - Set
+// console.log(list);
+// console.log(list.set(1, "WORLD!")); // True - set new value
+// console.log(list.set(34, "WORLD!")); // Index not found - false
+// console.log(list);
+
+// TEST - Insert
+// console.log(list.insert(1, "WORLD!")); // True - new value at position
+// console.log(list.get(1)); // new value WORLD at position 1
+// console.log(list.insert(0, "HEY!")); // True - insert first position
+// console.log(list.get(0));
+// console.log(list.insert(5, "END!")); // True insert last position
+// console.log(list.get(5));
+// console.log(list.insert(43, "END!")); // false - index is greater than length
+// console.log(list.insert(-3, "TEST")); // false - index is negative
+
+// Test - Remove
+// console.log(list.remove(1)); // should return "GOODBYE";
+// console.log(list);
+// console.log(list.remove(0)); // should return "HELLO";
+// console.log(list);
+// console.log(list.remove(23)); // should return undefined;
+// console.log(list);
+// console.log(list.remove(1)); // should return "!";
+// console.log(list);
+
+// test - reverse
+list.print();
+list.reverse();
+list.print();
--- section19_singly_linked_lists/singly_linked_list_own.js
+++ section19_singly_linked_lists/singly_linked_list_own.js
@@ -38,12 +38,12 @@ class SinglyLinkedList {
 	pop() {
 		let current;
 		let previous;
-		let value; // Value to pop
+		let node; // node to pop
 		if (this.head) {
 			current = this.head;
 			if (this.length === 1) {
 				// List only 1 item
-				value = this.head.val; // If only 1 item
+				node = this.head; // If only 1 item
 				this.head = null;
 				this.tail = null;
 			} else {
@@ -51,7 +51,7 @@ class SinglyLinkedList {
 					if (current.next) {
 						previous = current;
 					} else {
-						value = current.val; //assign value to return
+						node = current; //assign node to return
 					}
 					// previous = current;
 					current = current.next;
@@ -64,7 +64,7 @@ class SinglyLinkedList {
 			// Decrement length by one
 			this.length--;
 		}
-		return value;
+		return node;
 	}
 
 	shift() {
@@ -91,7 +91,8 @@ class SinglyLinkedList {
 	unshift(val) {
 		// Create a new node using the value
 		let newNode = new Node(val);
-		if (!this.head) { // Empty List
+		if (!this.head) {
+			// Empty List
 			// Set head a tail to new node
 			this.head = newNode;
 			this.tail = newNode;
@@ -106,6 +107,120 @@ class SinglyLinkedList {
 		// Return Linked List
 		return this;
 	}
+
+	get(index) {
+		let counter = 0;
+		let node;
+
+		if (index < 0 || index >= this.length) {
+			//invalid index
+			node = null;
+		} else {
+			node = this.head;
+			while (counter < index) {
+				//loop list, until index is found
+				node = node.next;
+				counter++;
+			}
+		}
+		return node;
+	}
+
+	set(index, value) {
+		// use get function to find specific node
+		let node = this.get(index);
+		let result = false; // if node not found, false
+
+		if (node) {
+			// Set new value
+			node.val = value;
+			result = true;
+		}
+
+		return result;
+	}
+
+	insert(index, value) {
+		let newNode;
+		let previousNode;
+		let result = false;
+		if (index >= 0 && index <= this.length) {
+			// index < 0 || index > length --> false
+			if (index === this.length) {
+				// if index is the same as length --> push
+				this.push(value);
+			} else if (index === 0) {
+				// if index is 0 --> unshift
+				this.unshift(value);
+			} else {
+				// access node at index -1 (previous node) --> get
+				previousNode = this.get(index - 1);
+				newNode = new Node(value);
+				// connect new node to old next
+				newNode.next = previousNode.next;
+				// set next property of previous node to be the new one
+				previousNode.next = newNode;
+				this.length++;
+			}
+			result = true;
+		}
+		return result;
+	}
+
+	remove(index) {
+		let node;
+		let previousNode;
+		if (index >= 0 && index < this.length) {
+			if (index === this.length - 1) {
+				// if index is the same as length -1 -> pop
+				node = this.pop();
+			} else if (index === 0) {
+				// if index is 0 -> shift
+				node = this.shift();
+			} else {
+				// use get to access index -1
+				previousNode = this.get(index - 1);
+				node = previousNode.next; // get node at index
+				// set next property of previous node
+				// to removed node next
+				previousNode.next = node.next;
+				this.length--;
+			}
+		}
+		return node;
+	}
+
+	reverse() {
+		let nextNode;
+		let previousNode;
+		let currentNode;
+
+		// start from beginning
+		previousNode = this.head;
+		
+		// swap head and tail
+		this.head = this.tail;
+		this.tail = previousNode;
+
+		// change next from tail
+		currentNode = previousNode.next;
+		this.tail.next = null;
+
+		for (let i = 1; i < this.length; i++) {
+			// save next node
+			nextNode = currentNode.next;
+			// replace the current evaluated node's next prop,
+			// should point to the previous node
+			currentNode.next = previousNode;
+			// move to the next node,
+			// 1 - replace previous for current
+			previousNode = currentNode;
+			// 2 - current is now the next node (stored in aux)
+			currentNode = nextNode;
+		}
+
+		return this;
+	}
 }
 
 // Test
@@ -113,6 +228,7 @@ class SinglyLinkedList {
 let list = new SinglyLinkedList();
 list.push("HELLO");
 list.push("GOODBYE");
+list.push("ALL");
 list.push("!");
 
 // Test - Push
@@ -140,6 +256,47 @@ list.push("!");
 // console.log(list);
 
 // Test - Unshift
-console.log(list);
-console.log(list.unshift("HI!"));
-console.log(list.unshift(123));
+// console.log(list);
+// console.log(list.unshift("HI!"));
+// console.log(list.unshift(123));
+
+// Test - Get
+// console.log(list.get(1));
+// console.log(list.get(0));
+// console.log(list.get(-1)); // index less than 0
+// console.log(list.get(4)); // index greater than length
+
+// Test - Set
+// console.log(list);
+// console.log(list.set(1, "WORLD!")); // True - set new value
+// console.log(list.set(34, "WORLD!")); // Index not found - false
+// console.log(list);
+
+// TEST - Insert
+// console.log(list.insert(1, "WORLD!")); // True - new value at position
+// console.log(list.get(1)); // new value WORLD at position 1
+// console.log(list.insert(0, "HEY!")); // True - insert first position
+// console.log(list.get(0));
+// console.log(list.insert(5, "END!")); // True insert last position
+// console.log(list.get(5));
+// console.log(list.insert(43, "END!")); // false - index is greater than length
+// console.log(list.insert(-3, "TEST")); // false - index is negative
+
+// Test - Remove
+// console.log(list.remove(1)); // should return "GOODBYE";
+// console.log(list);
+// console.log(list.remove(0)); // should return "HELLO";
+// console.log(list);
+// console.log(list.remove(23)); // should return undefined;
+// console.log(list);
+// console.log(list.remove(1)); // should return "!";
+// console.log(list);
+
+// Test - Reverse
+for (let i = 0; i < list.length; i++) {
+	console.log(list.get(i).val);
+}
+list.reverse();
+for (let i = 0; i < list.length; i++) {
+	console.log(list.get(i).val);
+}
